3c447daf304de1362912122d744d2ca2a37c61c0
quangcanh2975 <ubuntultc@gmail.com>
2018-10-20 10:16:48 +0700

    Initial Project

---
+++ AnimalsOOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AnimalsOOP
+{
+    public interface Animal
+    {
+        void say_hello();
+    }
+    public class Cat : Animal
+    {
+        public void say_hello()
+        {
+            Console.WriteLine("Hello, I'm Cat");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void say_hello()
+        {
+            Console.WriteLine("Hello, I'm Dog");
+        }
+    }
+    public class Zoo
+    {
+        private int animalAmount = 0;
+        public int GetAmount()
+        {
+            return animalAmount;
+        }
+        public bool AddAnimal()
+        {
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] zooAnimals = new Animal[100];
+            zooAnimals[0] = new Cat();
+            zooAnimals[1] = new Dog();
+            zooAnimals[0].say_hello();
+            zooAnimals[1].say_hello();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AnimalsOOP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AnimalsOOP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnimalsOOP")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fbcf5474-e231-4809-840a-64ac6a64da9b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
