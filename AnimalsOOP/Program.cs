6db008d5d6f6c6e12729620e2b9f07c2ac2749ed
quangcanh2975 <ubuntultc@gmail.com>
2018-10-22 12:42:23 +0700

    Change Zoo class

--- AnimalsOOP/Program.cs
+++ AnimalsOOP/Program.cs
@@ -20,17 +20,38 @@ namespace AnimalsOOP
             Console.WriteLine("Hello, I'm Dog");
         }
     }
+    public class Pig : Animal
+    {
+        public void say_hello()
+        {
+            Console.WriteLine("Hello, I'm Pig");
+        }
+    }
+    public class Chicken : Animal
+	{
+	public void say_hello()
+	{
+		Console.WriteLine("Hello, I'm Chicken");
+	}
+	}
+    
     public class Zoo
     {
         private int animalAmount = 0;
+	private Animal[] zooAnimals = new Animal[100];
         public int GetAmount()
         {
             return animalAmount;
         }
         public bool AddAnimal()
-        {
-            return true;
+	{	
+		
         }
+    	public bool RemoveAnimal()
+	{
+
+	}
+
     }
     class Program
     {
@@ -41,7 +62,6 @@ namespace AnimalsOOP
             zooAnimals[1] = new Dog();
             zooAnimals[0].say_hello();
             zooAnimals[1].say_hello();
-
             Console.ReadKey();
         }
     }
